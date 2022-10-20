674f0597d5887ffa6cffc2d293c3ecfee87c844e
dsergiod <destefanosergio@hotmail.com>
2022-10-20 18:14:24 -0300

    123

--- main.js
+++ main.js
@@ -1,30 +1,21 @@
-const compras = document.getElementById('compras');
-const btn2 = document.getElementById('btn2');
-const total = document.getElementById('total');
+// const compras = document.getElementById('compras');
+// const btn2 = document.getElementById('btn2');
+// const total = document.getElementById('total');
 const resumen = document.createElement('p');
 const opciones = document.getElementById('opciones');
 const cantidad = document.getElementById('cantidad');
 const ticket = 200;
 
 btn2.addEventListener('click', () => {
-    // console.log(opciones.value);
-    // console.log(cantidad.value);
-
     total.appendChild(resumen);
 
     if (opciones.value == 'Estudiante'){
-        console.log('cantidad ' + cantidad.value)
-        console.log('precio * ticket' + ticket);
         resumen.innerText = cantidad.value * (ticket - (ticket * 0.80));
     } 
     else if (opciones.value == 'Trainee'){
-        console.log('cantidad ' + cantidad.value)
-        console.log('precio ' + ticket);
         resumen.innerText = cantidad.value * (ticket - (ticket * 0.50));
     }    
     else if (opciones.value == 'Junior'){
-        console.log('cantidad ' + cantidad.value)
-        console.log('precio ' + ticket);
         resumen.innerText = cantidad.value * (ticket - (ticket * 0.15));
     }    
 });
